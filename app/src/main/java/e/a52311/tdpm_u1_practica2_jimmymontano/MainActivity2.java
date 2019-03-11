4e095d549bb34e8e61ec90f528ef5457be677922
jimmy montano <jianmontanolo@ittepic.edu.mx>
2019-03-11 10:38:32 -0700

    Practica 2 Unidad 1

---
+++ app/src/main/java/e/a52311/tdpm_u1_practica2_jimmymontano/MainActivity2.java
@@ -0,0 +1,13 @@
+package e.a52311.tdpm_u1_practica2_jimmymontano;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
